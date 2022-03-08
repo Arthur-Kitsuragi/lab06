573ea417a995514437225c478981e5dbc64ea8b9
Arthur-Kitsuragi <l-a-03@inbox.ru>
2022-03-08 22:19:02 +0300

    lab

---
+++ solver_application/CMakeFiles/solverlib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solverlib.dir/home/mat03/Arthur-Kitsuragi/workspace/projects/lab03/solver_lib/solver.cpp.o"
+  "CMakeFiles/solverlib.dir/home/mat03/Arthur-Kitsuragi/workspace/projects/lab03/solver_lib/solver.cpp.o.d"
+  "libsolverlib.a"
+  "libsolverlib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solverlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
